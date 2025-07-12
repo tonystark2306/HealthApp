5b1832a79407687cf2db21d230a9cd8904ac22d6
tonystark2306 <dat.ntt210177@sis.hust.edu.vn>
2025-07-12 09:00:03 +0700

    Initial Commit

---
+++ HealthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthApp
+//
+//  Created by iKame Elite Fresher 2025 on 7/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
